a871c968cadcf691dfee354fb206269a37af8cec
nmd9202@gmail.com <>
2022-09-18 15:27:05 +0200

    Finished speed slider tween

--- Worlds_Hardest_Maker/Assets/Scripts/UI/SpeedSliderAnim.cs
+++ Worlds_Hardest_Maker/Assets/Scripts/UI/SpeedSliderAnim.cs
@@ -6,13 +6,13 @@ public class SpeedSliderAnim : MonoBehaviour
 {
     private RectTransform rt;
     private UIFollowEntity follow;
-    private Animator anim;
+    private SpeedSliderTween anim;
 
     private void Start()
     {
         rt = GetComponent<RectTransform>();
         follow = GetComponent<UIFollowEntity>();
-        anim = GetComponent<Animator>();
+        anim = GetComponent<SpeedSliderTween>();
         
         Gone();
     }
@@ -20,21 +20,20 @@ public class SpeedSliderAnim : MonoBehaviour
     private void Update()
     {
         // set visible status (if no other slider is hovered)
-        Animator anim = GetComponent<Animator>();
-        bool hoveredHitbox = follow.entity.GetComponent<HoverSliderDetection>().MouseHoverSlider() && (!HoverSliderDetection.sliderHovered || anim.GetBool("Visible"));
+        bool hoveredHitbox = follow.entity.GetComponent<HoverSliderDetection>().MouseHoverSlider() && (!HoverSliderDetection.sliderHovered || anim.IsVisible());
 
         bool vis = !GameManager.Instance.Playing && Input.GetKey(GameManager.Instance.EditSpeedKey) && hoveredHitbox;
 
-        if (!vis && anim.GetBool("Visible")) HoverSliderDetection.sliderHovered = false;
+        if (!vis && anim.IsVisible()) HoverSliderDetection.sliderHovered = false;
 
-        anim.SetBool("Visible", vis);
+        anim.SetVisible(vis);
 
         if (vis && hoveredHitbox) HoverSliderDetection.sliderHovered = true;
     }
 
     public void Gone()
     {
-        if (!anim.GetBool("Visible"))
+        if (!anim.IsVisible())
         {
             follow.enabled = false;
             rt.position = new(2000, 2000);
