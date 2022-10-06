c0622b5304ff353319b5de96cdbe1a1d6b3b431d
nmd9202@gmail.com <>
2022-10-06 20:14:42 +0200

    removed ctrl + c = clear

--- Worlds_Hardest_Maker/Assets/Scripts/Game/KeyEvents.cs
+++ Worlds_Hardest_Maker/Assets/Scripts/Game/KeyEvents.cs
@@ -23,7 +23,6 @@ public class KeyEvents : MonoBehaviour
         {
             AlphaUITween anim = GameManager.Instance.Menu.GetComponent<AlphaUITween>();
             anim.SetVisible(!anim.IsVisible());
-            //GameManager.Instance.Menu.SetActive(!GameManager.Instance.Menu.activeSelf);
         }
 
         // teleport player to mouse pos
@@ -47,11 +46,11 @@ public class KeyEvents : MonoBehaviour
 #if !UNTIY_WEBGL
             if (Input.GetKeyDown(KeyCode.O)) GameManager.Instance.LoadLevel();
 #endif
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                if (GameManager.Instance.Multiplayer) view.RPC("ClearLevel", RpcTarget.All);
-                else GameManager.Instance.ClearLevel();
-            }
+            //if (Input.GetKeyDown(KeyCode.C))
+            //{
+            //    if (GameManager.Instance.Multiplayer) view.RPC("ClearLevel", RpcTarget.All);
+            //    else GameManager.Instance.ClearLevel();
+            //}
         }
 
         // check edit mode toggling if no ctrl and not playing
