969e9d1f5717b9caa1db02aba708383b18fab94b
nmd9202@gmail.com <>
2022-10-07 20:28:51 +0200

    <

--- Worlds_Hardest_Maker/Assets/Scripts/Tween/LevelSettingsPanelTween.cs
+++ Worlds_Hardest_Maker/Assets/Scripts/Tween/LevelSettingsPanelTween.cs
@@ -11,31 +11,17 @@ public class LevelSettingsPanelTween : MonoBehaviour
     [SerializeField] private AnimationCurve closeEase;
     [Space]
     [SerializeField] private float duration;
-    private float closedX;
-    private float width;
-    private bool open = false;
+    [SerializeField] private bool open = false;
+    [SerializeField] private float closedX;
 
     public void Toggle()
     {
         panel.DOKill();
-        if (open)
-        {
-            // close
-            // closed state -> x = closedX
-            panel.DOAnchorPosX(closedX, duration).SetEase(closeEase);
-        }
-        else
-        {
-            // open
-            // opened state -> x = closedX + width
-            panel.DOAnchorPosX(closedX + width, duration).SetEase(openEase);
-        }
-        open = !open;
-    }
 
-    private void Start()
-    {
-        closedX = panel.anchoredPosition.x;
-        width = panel.rect.width;
+        // closed state -> x = closedX
+        // opened state -> x = closedX + width
+        panel.DOAnchorPosX(open ? closedX : closedX + panel.rect.width, duration).SetEase(open ? closeEase : openEase);
+
+        open = !open;
     }
 }
