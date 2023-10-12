18f5d2dcd81d6d6d6068d36b749f2e3977640fa1
Bluestimes <nikom-d@t-online.de>
2023-10-12 22:25:54 +0200

    Fixed anchor ball deleting bug

--- Worlds_Hardest_Maker/Assets/Scripts/LevelObjects/Entity/Ball/Anchor/AnchorBall/AnchorBallController.cs
+++ Worlds_Hardest_Maker/Assets/Scripts/LevelObjects/Entity/Ball/Anchor/AnchorBall/AnchorBallController.cs
@@ -32,5 +32,7 @@ public class AnchorBallController : EntityController
         {
             AnchorBallManager.Instance.AnchorBallListGlobal.Remove(this);
         }
+
+        Destroy(transform.parent.gameObject);
     }
 }
