f3559398a7aafb5ce3ba56ad5fdf65620ab13d57
Lexderi <adrian@rummert.de>
2023-11-02 17:15:47 +0100

    Added option to reset stats

--- Worlds_Hardest_Maker/Assets/Scripts/UI/Level Hub/LevelModifyController.cs
+++ Worlds_Hardest_Maker/Assets/Scripts/UI/Level Hub/LevelModifyController.cs
@@ -1,22 +1,24 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using MyBox;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelModifyController : MonoBehaviour
 {
     public static LevelModifyController Instance { get; private set; }
 
     [HideInInspector] public LevelCardController CurrentCard;
-
+    
     [Separator("References")] [SerializeField] [InitializationField] [MustBeAssigned] private TMP_InputField levelNameText;
     [SerializeField] [InitializationField] [MustBeAssigned] private TMP_InputField descriptionText;
     [SerializeField] [InitializationField] [MustBeAssigned] private TMP_InputField creatorText;
     [Space] [InitializationField] [MustBeAssigned] public MoveRelativeTween LevelModifyStartTween;
     [InitializationField] [MustBeAssigned] public MoveRelativeTween LevelModifyBackTween;
+    [SerializeField] [InitializationField] [MustBeAssigned] private Toggle resetPlayStats;
+    [SerializeField] [InitializationField] [MustBeAssigned] private Toggle resetEditorStats;
 
     public void SaveLevelSettings()
     {
@@ -37,6 +39,19 @@ public class LevelModifyController : MonoBehaviour
         levelData.Info.Description = newDescription;
         levelData.Info.Creator = newCreator;
 
+        if (resetPlayStats.isOn)
+        {
+            levelData.Info.Completions = 0;
+            levelData.Info.Deaths = 0;
+            levelData.Info.PlayTime = TimeSpan.Zero;
+            levelData.Info.BestCompletionTime = TimeSpan.MaxValue;
+        }
+
+        if (resetEditorStats.isOn)
+        {
+            levelData.Info.EditTime = TimeSpan.Zero;
+        }
+
         // overwrite file with new description and creator
         SaveSystem.SerializeLevelData(newPath, levelData);
 
@@ -48,6 +63,9 @@ public class LevelModifyController : MonoBehaviour
         levelNameText.text = card.Name;
         descriptionText.text = card.Description;
         creatorText.text = card.Creator.Substring(3, card.Creator.Length - 3);
+
+        resetPlayStats.isOn = false;
+        resetEditorStats.isOn = false;
     }
 
     private void Awake()
