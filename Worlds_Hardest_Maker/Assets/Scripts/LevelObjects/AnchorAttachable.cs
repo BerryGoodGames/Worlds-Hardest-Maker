af1688a94574cad67cb90a900ee51812af5b1638
Bluestimes <nikom-d@t-online.de>
2024-05-17 17:09:20 +0200

    Fixed sorting layer stuff

--- Worlds_Hardest_Maker/Assets/Scripts/LevelObjects/AnchorAttachable.cs
+++ Worlds_Hardest_Maker/Assets/Scripts/LevelObjects/AnchorAttachable.cs
@@ -8,7 +8,7 @@ public class AnchorAttachable : MonoBehaviour
 {
     [InitializationField] [Required] public SpriteRenderer MainSprite;
     [InitializationField] public bool HasSortingGroup;
-    [EnableIf(nameof(HasSortingGroup))] [InitializationField] [CanBeNull] public SortingGroup SortingGroup;
+    [ConditionalField(nameof(HasSortingGroup))] [InitializationField] [CanBeNull] public SortingGroup SortingGroup;
     [InitializationField] public bool HasOutline;
-    [EnableIf(nameof(HasOutline))] [InitializationField] [Required] public FieldOutline OutlineComp;
+    [ConditionalField(nameof(HasOutline))] [InitializationField] [Required] public FieldOutline OutlineComp;
 }
