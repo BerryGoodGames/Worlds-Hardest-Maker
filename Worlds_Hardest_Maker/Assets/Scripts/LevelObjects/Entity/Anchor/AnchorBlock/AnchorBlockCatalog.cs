99a049fdd6349a6b5b62a094609d88dc6d17079b
LAPTOP-NKMDTL\nikom <nikom-d@t-online.de>
2026-08-22 00:25:25 +0200

    refactor: add anchor block definition and catalog objects

---
+++ Worlds_Hardest_Maker/Assets/Scripts/LevelObjects/Entity/Anchor/AnchorBlock/AnchorBlockCatalog.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnchorBlockCatalog", menuName = "ScriptableObjects/Anchor Blocks/Catalog")]
+public class AnchorBlockCatalog : ScriptableObject
+{
+    public List<AnchorBlockDefinition> Definitions;
+}
---
+++ Worlds_Hardest_Maker/Assets/Scripts/LevelObjects/Entity/Anchor/AnchorBlock/AnchorBlockDefinition.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAnchorBlock", menuName = "ScriptableObjects/Anchor Blocks/Definition")]
+public class AnchorBlockDefinition : ScriptableObject
+{
+    public string TypeID;
+    public GameObject Prefab;
+}
