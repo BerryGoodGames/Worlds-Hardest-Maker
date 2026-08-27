8b536cfa4fedf05cc353469fa3dbbd5de9e2b667
LAPTOP-NKMDTL\nikom <nikom-d@t-online.de>
2026-08-27 21:49:36 +0200

    fix[new]: anchor panel opens after position edit

---
+++ Worlds_Hardest_Maker/Assets/Scripts/Panels/ReopenAfterPositionEditPanel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using VContainer;
+
+namespace WorldsHardestMaker.Panels
+{
+    [RequireComponent(typeof(PanelController))]
+    public class ReopenAfterPositionEditPanel : MonoBehaviour
+    {
+        private PanelController panelController;
+
+        private bool wasOpenBeforeEdit;
+
+        private EventBus eventBus;
+        [Inject] private IPanelService panelService;
+
+        [Inject]
+        private void Construct(EventBus eventBus)
+        {
+            this.eventBus = eventBus;
+            
+            eventBus.Subscribe<AnchorPositionEditStartedEvent>(OnAnchorPositionEditStarted);
+            eventBus.Subscribe<AnchorPositionEditEndedEvent>(OnAnchorPositionEditEnded);
+        }
+
+        private void OnDestroy()
+        {
+            eventBus.Unsubscribe<AnchorPositionEditStartedEvent>(OnAnchorPositionEditStarted);
+            eventBus.Unsubscribe<AnchorPositionEditEndedEvent>(OnAnchorPositionEditEnded);
+        }
+
+        private void Awake()
+        {
+            panelController = GetComponent<PanelController>();
+        }
+
+        private void OnAnchorPositionEditStarted(AnchorPositionEditStartedEvent evt)
+        {
+            wasOpenBeforeEdit = panelController.IsOpen;
+        }
+        
+        private void OnAnchorPositionEditEnded(AnchorPositionEditEndedEvent evt)
+        {
+            if (wasOpenBeforeEdit)
+            {
+                panelService.SetPanelOpen(panelController, true);
+            }
+        }
+    }
+}
